b557609136c39f262b2ecfd737e5025e4f448cf8
belle-brt <148067383+belle-brt@users.noreply.github.com>
2023-10-16 11:06:42 -0300

    Update triangulo.py

--- triangulo.py
+++ triangulo.py
@@ -5,3 +5,16 @@ print('Para formar um Triangulo: ')
 ladoA = int(input('Digite o tamanho do lado A: '))
 ladoB = int(input('Digite o tamanho do lado B: '))
 ladoC = int(input('Digite o tamanho do lado C: '))
+def triangulo(a, b, c):
+  if a + b > c and a + c > b and b + c > a:
+    return True
+  else:
+    return False
+
+def tipo_triangulo(a, b, c):
+  if a == b == c:
+    return "equilátero"
+  elif a == b or a == c or b == c:
+    return "isósceles"
+  else:
+    return "escaleno"
