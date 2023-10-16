1cf2be3d35223234ab863f89e4557dbe6a0410d1
lahppivato <130668198+lahppivato@users.noreply.github.com>
2023-10-16 10:59:33 -0300

    triangulo

---
+++ triangulo.py
@@ -0,0 +1,7 @@
+# crie um prog que leia os 3 lados de um triangulo
+# verificar se os lados de um polígono formam um triangulo
+# informar o tipo de triangulo formado
+print('Para formar um Triangulo: ')
+ladoA = int(input('Digite o tamanho do lado A: '))
+ladoB = int(input('Digite o tamanho do lado B: '))
+ladoC = int(input('Digite o tamanho do lado C: '))
